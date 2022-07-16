39c1f68f62c828126afedf93fb5b1be633d48161
yuvalha96 <yuvalha@wix.com>
2022-07-16 15:11:20 +0300

    sms permission request

--- app/src/main/java/com/example/finaleProject/MainActivity.java
+++ app/src/main/java/com/example/finaleProject/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.finaleProject;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.Application;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.fragment.app.Fragment;
@@ -17,6 +22,9 @@ import androidx.fragment.app.FragmentTransaction;
 
 
 public class MainActivity extends AppCompatActivity implements NotesAdapter.INotesAdapterListener {
+
+    private static final int RECEIVE_SMS_REQUEST_CODE   = 1;
+    private static final int READ_SMS_REQUEST_CODE      = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,9 @@ public class MainActivity extends AppCompatActivity implements NotesAdapter.INot
         ActionBar menu = getSupportActionBar();
         menu.setDisplayShowHomeEnabled(true);
         menu.setDisplayUseLogoEnabled(true);
+
+        askForSmsDangerousPermissions();
+
     }
 
     @Override
@@ -101,5 +112,42 @@ public class MainActivity extends AppCompatActivity implements NotesAdapter.INot
             setPreferencesFromResource(R.xml.settings_pref, rootKey);
         }
 
+    }
+
+    private void askForSmsDangerousPermissions() {
+        requestSmsDangerousPermission(Manifest.permission.READ_SMS, READ_SMS_REQUEST_CODE);
+        requestSmsDangerousPermission(Manifest.permission.RECEIVE_SMS, RECEIVE_SMS_REQUEST_CODE);
+    }
+
+    private void requestSmsDangerousPermission(String permission, int permissionRequestCode)
+    {
+        // check if permission already granted
+        if (ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        // Permission is not granted. show an explanation.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission))
+            Toast.makeText(this, "You must grant this permission in order to see SMS messages", Toast.LENGTH_LONG).show();
+
+        // request the permission
+        ActivityCompat.requestPermissions(this, new String[] { permission }, permissionRequestCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length == 0)
+            return;
+
+        boolean firstPermissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+        switch (requestCode) {
+            case RECEIVE_SMS_REQUEST_CODE:
+                Toast.makeText(this, "RECEIVE_SMS permission granted: " + firstPermissionGranted, Toast.LENGTH_SHORT).show();
+                break;
+            case READ_SMS_REQUEST_CODE:
+                Toast.makeText(this, "READ_SMS permission granted: " + firstPermissionGranted, Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
